7359dba981fad13680a9dbe0f7f05681d51e71ca
Fred <fred@localhost.localdomain>
2025-01-14 10:46:01 -0600

    added django to run sudo without password and edited clean.sh script

--- clean.sh
+++ clean.sh
@@ -3,4 +3,12 @@
 sudo yum clean all
 sudo rm -f /var/log/*.log*
 sudo rm -rf /tmp/*
-docker system prune -a --volumes
+
+# Check for --prune-volumes argument
+if [[ "$1" == "--prune-volumes" ]]; then
+    echo "Pruning Docker system, including volumes..."
+    docker system prune -a --volumes
+else
+    echo "Pruning Docker system without volumes..."
+    docker system prune -a
+fi
